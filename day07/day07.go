b8354a6dc3148a3498a345ad956c0618b4882e5a
Stephen Thirlwall <sdt@dr.com>
2021-12-07 16:18:39 +1100

    Day 7 part 1

---
+++ day07/day07.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("usage: ", os.Args[0], " input-file")
+	}
+
+	positions := getInput(os.Args[1])
+
+	fmt.Println(part1(positions))
+}
+
+func part1(positions []int) int {
+	min := math.MaxInt
+	max := math.MinInt
+
+	for _, position := range positions {
+		if position < min {
+			min = position
+		}
+		if position > max {
+			max = position
+		}
+	}
+
+	leastTotal := math.MaxInt
+
+	for dest := min; dest <= max; dest++ {
+		total := getTotalMovement(positions, dest)
+		if total < leastTotal {
+			leastTotal = total
+		}
+	}
+	return leastTotal
+}
+
+func getTotalMovement(positions []int, dest int) int {
+	total := 0
+	for _, position := range positions {
+		total += abs(position - dest)
+	}
+	return total
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func getInput(filename string) []int {
+	words := strings.Split(getInputLines(filename)[0], ",")
+
+	positions := make([]int, len(words))
+	for i, word := range words {
+		value, err := strconv.Atoi(word)
+		if err != nil {
+			log.Fatal(err)
+		}
+		positions[i] = value
+	}
+	return positions
+}
+
+func getInputLines(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := make([]string, 0)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
