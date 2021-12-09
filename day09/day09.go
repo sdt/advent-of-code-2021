30f5dd717f53fa15437182d3b0c77f64af42f6a9
Stephen Thirlwall <sdt@dr.com>
2021-12-09 17:15:49 +1100

    Day 9 part 2

--- day09/day09.go
+++ day09/day09.go
@@ -3,11 +3,12 @@ package main
 import (
 	"advent-of-code/common"
 	"fmt"
+	"sort"
 )
 
 type HeightMap struct {
 	rows, cols int
-	height []int
+	height     []int
 }
 
 func main() {
@@ -15,6 +16,7 @@ func main() {
 	heightMap := parseHeightMap(filename)
 
 	fmt.Println(part1(&heightMap))
+	fmt.Println(part2(&heightMap))
 }
 
 func part1(h *HeightMap) int {
@@ -27,15 +29,29 @@ func part1(h *HeightMap) int {
 	return total
 }
 
+func part2(h *HeightMap) int {
+	sizes := make([]int, 0)
+
+	for row := 0; row < h.rows; row++ {
+		for col := 0; col < h.cols; col++ {
+			sizes = append(sizes, h.FloodFill(row, col))
+		}
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(sizes)))
+
+	return sizes[0] * sizes[1] * sizes[2]
+}
+
 func parseHeightMap(filename string) HeightMap {
 	lines := common.GetInputLines(filename)
 	rows := len(lines)
 	cols := len(lines[0])
-	heightMap := HeightMap{ rows:rows, cols:cols, height: make([]int, rows * cols) }
+	heightMap := HeightMap{rows: rows, cols: cols, height: make([]int, rows*cols)}
 
 	index := 0
-	for _, line := range(lines) {
-		for _, digit := range(line) {
+	for _, line := range lines {
+		for _, digit := range line {
 			heightMap.height[index] = int(digit - '0')
 			index++
 		}
@@ -49,13 +65,13 @@ func (h *HeightMap) Height(row, col int) int {
 		return 9
 	}
 
-	index := row * h.cols + col
+	index := row*h.cols + col
 	return h.height[index]
 }
 
 func (h *HeightMap) IsLowPoint(row, col int) bool {
 	height := h.Height(row, col)
-	return height < h.Height(row - 1, col) && height < h.Height(row + 1, col) && height < h.Height(row, col - 1) && height < h.Height(row, col + 1)
+	return height < h.Height(row-1, col) && height < h.Height(row+1, col) && height < h.Height(row, col-1) && height < h.Height(row, col+1)
 }
 
 func (h *HeightMap) RiskLevel(row, col int) int {
@@ -64,4 +80,15 @@ func (h *HeightMap) RiskLevel(row, col int) int {
 	}
 
 	return h.Height(row, col) + 1
+}
+
+func (h *HeightMap) FloodFill(row, col int) int {
+	if h.Height(row, col) == 9 {
+		return 0
+	}
+
+	index := row*h.cols + col
+	h.height[index] = 9
+
+	return 1 + h.FloodFill(row-1, col) + h.FloodFill(row+1, col) + h.FloodFill(row, col-1) + h.FloodFill(row, col+1)
 }
