5fd596fff3adc4ecac2dc96edbae0c3e9c5edb8d
Stephen Thirlwall <sdt@dr.com>
2021-12-20 16:00:52 +1100

    Day 15 finally
    Use a priority queue, and finish as soon as you reach the end.
    Best first search means the first time we get to the end will be the
    best time!

--- day15/day15.go
+++ day15/day15.go
@@ -3,6 +3,7 @@ package main
 import (
 	"advent-of-code/aoc"
 	"fmt"
+	"math"
 )
 
 type Cavern struct {
@@ -25,64 +26,45 @@ func main() {
 }
 
 func part1(cavern *Cavern) int {
-	lowest := cavern.makeGrid(9999)
+	lowest := cavern.makeGrid(math.MaxInt)
 
 	//printGrid(cavern.rows + 2, cavern.cols + 2, cavern.riskLevel)
 	//printGrid(cavern.rows + 2, cavern.cols + 2, lowest)
 
-	posStack := make([]int, cavern.rows * cavern.cols)
-	stackTop := 0
 	w := cavern.cols + 2
+	h := cavern.rows + 2
 	start := w + 1
+	end := w * (h-1) - 2
+
+	pathQueue := MakePathQueue()
+	pathQueue.Insert(MakePath(start, 0))
 
 	lowest[start] = 0
-	posStack[stackTop] = start
-	stackTop++
 
 	hDelta := 1
 	vDelta := cavern.cols + 2
+	deltas := [...]int{ hDelta, vDelta, -hDelta, -vDelta }
 
-	for stackTop > 0 {
-		stackTop--
-		pos := posStack[stackTop]
-		pathRiskLevel := lowest[pos]
+	for {
+		path, ok := pathQueue.Remove()
+		if !ok {
+			panic("All out of bubblegum!")
+		}
 
-		p := pos + hDelta
-		if nextRiskLevel := cavern.riskLevel[p]; nextRiskLevel > 0 {
-			if riskLevel := pathRiskLevel + nextRiskLevel; riskLevel < lowest[p] {
-				lowest[p] = riskLevel
-				posStack[stackTop] = p
-				stackTop++
-			}
-		}
-		p = pos + vDelta
-		if nextRiskLevel := cavern.riskLevel[p]; nextRiskLevel > 0 {
-			if riskLevel := pathRiskLevel + nextRiskLevel; riskLevel < lowest[p] {
-				lowest[p] = riskLevel
-				posStack[stackTop] = p
-				stackTop++
-			}
-		}
-		p = pos - hDelta
-		if nextRiskLevel := cavern.riskLevel[p]; nextRiskLevel > 0 {
-			if riskLevel := pathRiskLevel + nextRiskLevel; riskLevel < lowest[p] {
-				lowest[p] = riskLevel
-				posStack[stackTop] = p
-				stackTop++
-			}
-		}
-		p = pos - vDelta
-		if nextRiskLevel := cavern.riskLevel[p]; nextRiskLevel > 0 {
-			if riskLevel := pathRiskLevel + nextRiskLevel; riskLevel < lowest[p] {
-				lowest[p] = riskLevel
-				posStack[stackTop] = p
-				stackTop++
+		for _, delta := range deltas {
+			p := path.pos + delta
+			if nextRiskLevel := cavern.riskLevel[p]; nextRiskLevel > 0 {
+				if riskLevel := path.cost + nextRiskLevel; riskLevel < lowest[p] {
+					if p == end {
+						return riskLevel
+					}
+					lowest[p] = riskLevel
+					pathQueue.Insert(MakePath(p, riskLevel))
+				}
 			}
 		}
 	}
 
-	h := cavern.rows + 2
-	end := w * (h-1) - 2
 	return lowest[end]
 }
 
@@ -157,4 +139,80 @@ func printGrid(rows, cols int, cell []int) {
 		fmt.Printf("\n")
 	}
 	fmt.Printf("\n")
+}
+
+//------------------------------------------------------------------------------
+
+type Path struct {
+	pos, cost int
+}
+
+func MakePath(pos, cost int) Path {
+	return Path{pos: pos, cost: cost}
+}
+
+type PathQueue struct {
+	queue []Path
+}
+
+func MakePathQueue() PathQueue {
+	return PathQueue{queue: make([]Path, 0)}
+}
+
+func (this *PathQueue) Insert(path Path) {
+	this.queue = append(this.queue, path)
+
+	// Upheap
+	for child := len(this.queue) - 1; child > 0; {
+		parent := (child - 1) / 2
+		if this.queue[parent].cost <= path.cost {
+			return
+		}
+
+		this.queue[child] = this.queue[parent]
+		this.queue[parent] = path
+		child = parent
+	}
+}
+
+func (this *PathQueue) Remove() (Path, bool) {
+	if len(this.queue) == 0 {
+		return Path{}, false
+	}
+
+	ret := this.queue[0]
+	size := len(this.queue) - 1
+
+	if size > 0 {
+		this.queue[0] = this.queue[size]
+
+		// Downheap
+		parent := 0
+		for {
+			lchild := parent * 2 + 1
+			if lchild >= size {
+				break
+			}
+			rchild := parent * 2 + 2
+
+			var child int
+			if rchild >= size {
+				child = lchild
+			} else if this.queue[lchild].cost <= this.queue[rchild].cost {
+				child = lchild
+			} else {
+				child = rchild
+			}
+
+			if this.queue[parent].cost < this.queue[child].cost {
+				break
+			}
+
+			this.queue[parent], this.queue[child] = this.queue[child], this.queue[parent]
+			parent = child
+		}
+	}
+
+	this.queue = this.queue[0:size]
+	return ret, true
 }
