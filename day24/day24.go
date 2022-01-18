5500fea234ce1f0ab234417aada9bc317f8a7894
Stephen Thirlwall <sdt@dr.com>
2022-01-18 17:13:03 +1100

    Recursive depth-first implementation of day 24

--- day24/day24.go
+++ day24/day24.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
+type Word int64
 type Params struct {
 	a, b, c Word
 }
@@ -27,100 +27,63 @@ var params = [...]Params{
 }
 
 func main() {
-	lowest, highest := solve()
-	fmt.Println(highest)
-	fmt.Println(lowest)
+	// 91398299697996
+	// 41171183141291
+	fmt.Println(part1())
+	fmt.Println(part2())
 }
 
-func solve() (string, string) {
-	partials := []Partial{Partial{}}
-	from := 0
-	for {
-		size := chunkSize(from)
-		if size == 0 {
-			break
+func part1() string {
+	return solve(func (i int) Word {
+		return Word(9 - i)
+	})
+}
+
+func part2() string {
+	return solve(func (i int) Word {
+		return Word(i + 1)
+	})
+}
+
+
+func solve(f func(int)Word) string {
+	solution := make([]byte, len(params))
+
+	inputs := make([]Word, 9)
+	for i := 0; i < len(inputs); i++ {
+		inputs[i] = f(i)
+	}
+
+	recurse(solution, inputs, 0, 0)
+
+	return string(solution)
+}
+
+func recurse(solution[] byte, inputs[] Word, depth int, z Word) bool {
+	if depth == len(params) {
+		return true
+	}
+
+	p := &params[depth]
+
+	for _, input := range inputs {
+		nextZ, ok := NativeChunk(input, p, z)
+		if (ok || p.a == 1) && recurse(solution, inputs, depth+1, nextZ) {
+			solution[depth] = '0' + byte(input)
+			return true
 		}
-
-		next := []Partial{}
-		for _, partial := range partials {
-			next = append(next, TestPrefixes(partial, size)...)
-		}
-
-		//fmt.Printf("From=%d size=%d remaining=%d\n", from, size, len(next))
-		from += size
-		partials = next
 	}
-	return partials[0].input, partials[len(partials)-1].input
+	return false
 }
 
-func chunkSize(from int) int {
-	if from == len(params) {
-		return 0
-	}
-
-	i := 0
-	for params[from+i].a == 1 {
-		i++
-	}
-	return i + 1
-}
-
-type Word int64
 
 // a: line  5
 // b: line  6
 // c: line 16
 
-func pow(base, exponent int) int {
-	ret := 1
-	for i := 0; i < exponent; i++ {
-		ret *= base
-	}
-	return ret
-}
-
-type Partial struct {
-	input string
-	z     Word
-}
-
-func TestPrefixes(partial Partial, more int) []Partial {
-	count := pow(9, more)
-	out := make([]Partial, 0)
-
-	offset := len(partial.input)
-
-	for i := 0; i < count; i++ {
-		input := convert(Word(i), more)
-
-		keep := false
-		z := partial.z
-		for j := 0; j < more; j++ {
-			k := offset + j
-			z, keep = NativeChunk(input[j], &params[k], z)
-		}
-		if keep {
-			out = append(out, Partial{input: partial.input + input, z: z})
-			//fmt.Println(input, z)
-		}
-	}
-	return out
-}
-
-func NativeChunk(input byte, p *Params, z Word) (Word, bool) {
-	w := Word(input - '0')
-	if z%26 != w-p.b {
-		return (z/p.a)*26 + w + p.c, false
+func NativeChunk(input Word, p *Params, z Word) (Word, bool) {
+	if z%26 != input-p.b {
+		return (z/p.a)*26 + input + p.c, false
 	}
 	return z / p.a, true
-}
-
-func convert(value Word, width int) string {
-	format := fmt.Sprintf("%%0%ds", width)
-	digits := fmt.Sprintf(format, strconv.FormatInt(int64(value), 9))
-	number := make([]byte, len(digits))
-	for i, digit := range digits {
-		number[i] = byte(digit) + 1
-	}
-	return string(number)
 }
